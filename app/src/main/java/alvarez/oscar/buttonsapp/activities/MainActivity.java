435d1e41b08a2eee6dd1a157a567b0a2acb5b142
Oscar Alvarez <oscar.alvarez@globant.com>
2018-09-01 20:16:53 -0500

    Add data binding on main view

--- app/src/main/java/alvarez/oscar/buttonsapp/activities/MainActivity.java
+++ app/src/main/java/alvarez/oscar/buttonsapp/activities/MainActivity.java
@@ -1,12 +1,14 @@
 package alvarez.oscar.buttonsapp.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import javax.inject.Inject;
 
 import alvarez.oscar.buttonsapp.ButtonsApplication;
 import alvarez.oscar.buttonsapp.R;
+import alvarez.oscar.buttonsapp.databinding.ActivityMainBinding;
 import alvarez.oscar.buttonsapp.presenters.MainPresenter;
 
 /**
@@ -19,13 +21,13 @@ public class MainActivity extends AppCompatActivity {
     @Inject
     MainPresenter mPresenter;
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         ButtonsApplication.getComponent().inject(this);
-
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
     }
 }
